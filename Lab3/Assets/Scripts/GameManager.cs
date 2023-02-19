750ae236015cdd925d5a295dd8702143bd098c74
RhettBooher13 <rhettbooher@gmail.com>
2023-02-19 14:14:02 -0600

    Started menu.

--- Lab3/Assets/Scripts/GameManager.cs
+++ Lab3/Assets/Scripts/GameManager.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
 
-
+    
     public static GameManager Instance {get; private set;}
 
     public GameObject dialogBox;
     public TextMeshProUGUI dialogText;
 
-    public float x;
-    public float y;
+    public GameObject curtain;
+    private bool raiseLower = false;
+
+
+    public GameObject canvas;
+    public GameObject eventSystem;
+
+    public GameObject mainScreen;
 
     public void DialogShow(string text) {
         dialogBox.SetActive(true);
@@ -37,16 +45,75 @@ public class GameManager : MonoBehaviour
         if (Instance == null) {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(canvas);
+            DontDestroyOnLoad(eventSystem);
         } else {
             Destroy(gameObject);
         }
 
     }
 
+
+    IEnumerator ColorLerpFunction(bool fadeout, float duration)
+    {
+        float time = 0;
+        raiseLower = true;
+        Image curtainImg = curtain.GetComponent<Image>();
+        Color startValue;
+        Color endValue;
+        if (fadeout) {
+            startValue = new Color(0, 0, 0, 0);
+            endValue = new Color(0, 0, 0, 1);
+        } else {
+            startValue = new Color(0, 0, 0, 1);
+            endValue = new Color(0, 0, 0, 0);
+        }
+
+        while (time < duration)
+        {
+            curtainImg.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        curtainImg.color = endValue;
+        raiseLower = false;
+    }
+
+
+     IEnumerator LoadYourAsyncScene(string scene)
+     {
+        // The Application loads the Scene in the background as the current Scene runs.
+        // This is particularly good for creating loading screens.
+        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+        // a sceneBuildIndex of 1 as shown in Build Settings.
+
+        StartCoroutine(ColorLerpFunction(true, 1));
+
+        while (raiseLower)
+        {
+            yield return null;
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scene);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        StartCoroutine(ColorLerpFunction(false, 1));
+    }
+    public void StartGame() {
+        StartCoroutine(LoadYourAsyncScene("SampleScene"));
+        mainScreen.SetActive(false);
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        
     }
 
     // Update is called once per frame
---
+++ Lab3/Assets/Scripts/StartButton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+
+
+{
+
+
+    public void startGame() {
+        
+    }
+    
+}
